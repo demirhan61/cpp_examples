6748e657b9a75acb3826e472fed782a04fc95996
demirhan61 <36771454+demirhan61@users.noreply.github.com>
2018-03-02 19:11:05 +0300

    Update bolensayisi.cpp

--- bolensayisi.cpp
+++ bolensayisi.cpp
@@ -1,4 +1,16 @@
 int bolensayisi(int sayi){
+/* Kullanımı
+
+	int main(){
+		int sayi,bs;
+		cout<<"sayi giriniz:";
+		cin>>sayi;
+		bs=bolensayisi(sayi);
+		cout<<"sayinin bolenlerinin sayisi:"<<bs<<endl;
+		return 0;	
+	}
+
+*/	
 	// buraya sayi değiskeninin bolen sayisini bulan kod
 	int adet=0;
 	for(int i=1;i<=sayi;i++){
